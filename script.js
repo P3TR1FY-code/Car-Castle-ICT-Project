070ac039e4d4b56f070e3586bbd64726ed22b895
P3TR1FY-code <muhammad.haider07866@gmail.com>
2025-12-25 21:44:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function login() {
+    alert("Login Successful");
+    window.location.href = "home.html";
+}
+
+function signup() {
+    alert("Signup Successful");
+    window.location.href = "home.html";
+}
+
+function goBack() {
+    window.location.href = "categories.html";
+}
+function goBack() {
+    window.location.href = "categories.html";
+}
